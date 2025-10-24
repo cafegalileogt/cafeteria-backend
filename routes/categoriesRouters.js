26c3c304aa6cf173686fa1e58480053839a39e13
Randol Gutierrez <randolg@GBODEGAS.INTERNO>
2025-10-24 15:14:22 -0600

    se crearon endpoint para CRUD de categorias y para obtener categorias en Home y panel admin

---
+++ routes/categoriesRouters.js
@@ -0,0 +1,15 @@
+const { authUser } = require('../controllers/authController');
+const { getCategories, getCategoriesHome, getCategoriesById, postCategory, updateCategory, deleteCategory} = require("../controllers/categoriesController");
+
+const router = require('express').Router();
+
+
+router.get("/getCategories", authUser, getCategories);
+router.get("/getCategoriesHome", authUser, getCategoriesHome);
+router.get("/getCategories/:idCategorie", authUser, getCategoriesById);
+router.post("/postCategory", authUser, postCategory);
+router.patch("/updateCategory/:id_categoria", authUser, updateCategory);
+router.delete("/deleteCategory/:id_categoria", authUser, deleteCategory);
+
+
+module.exports = router;
--- routes/productsRouters.js
+++ routes/productsRouters.js
@@ -8,8 +8,6 @@ router.get("/product_by_id/:id_producto", authUser, getProductById);
 router.get("/productos_by_categorias/:idCategorie", authUser, filtrarByCategories);
 router.get("/productos_categorias", authUser, AllByCategories);
 router.patch("/update_product/:id_producto", authUser, updateProduct);
-router.delete("/delete_product/:id_producto", authUser, deleteteProduct);
-router.get("/getCategories", getCategories);
-
+router.delete("/delete_product/:id_producto", authUser, deleteteProduct)
 
 module.exports = router;
